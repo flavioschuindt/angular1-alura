bb104fde4a8adea55d1e002e7d0f786e41cd23b3
Flavio Schuindt <flavio.schuindt@ge.com>
2017-10-08 01:04:43 -0300

    - Finish class 6

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -15,5 +15,21 @@ angular.module("minhasDiretivas", [])
 
 	return ddo;
 
+})
+.directive('minhaFoto', function() {
+
+    var ddo = {};
+
+    ddo.restrict = "AE";
+
+    ddo.scope = {
+        titulo: '@',
+        url: '@'
+    };
+
+    ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+
+    return ddo;
+
 });
 
